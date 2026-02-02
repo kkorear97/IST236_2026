8fc71309daa4a0401e2e4797c0a281639a83a99a
Kylie <kkorear97@gmail.com>
2026-02-02 14:03:04 -0500

    Adding DiceRollApp

---
+++ Week3/DiceRollApp/App.js
@@ -0,0 +1,246 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Pressable, Modal } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import { TextInput } from "react-native";
+
+export default function App() {
+  //Set a Max and Min dice value
+  const maxVal = 6;
+  const minVal = 1;
+  //Create state management variables
+  const [dice1, setDice1] = useState(1);
+  const [dice2, setDice2] = useState(1);
+  const [modalIsVisible , setModalIsVisible] = useState(false);
+  const [userGuess, setUserGuess] = useState("");
+  const [userWager, setUserWager] = useState("");
+  const [diceSum, setDiceSum] = useState(0);
+
+  function startDiceRollHandler() {
+    setModalIsVisible(true);
+    setUserGuess("");
+    setUserWager("");
+  }
+
+  function endDiceRollHandler() {
+    setModalIsVisible(false);
+  }
+
+  function onDiceRoll() {
+    const rndNum1 = Math.floor(Math.random() * (maxVal -minVal)) + minVal;
+    const rndNum2 = Math.floor(Math.random() * (maxVal -minVal)) + minVal;
+    setDice1(rndNum1);
+    setDice2(rndNum2);
+
+    let result = rndNum1 + rndNum2;
+    setDiceSum(result);
+
+    endDiceRollHandler();
+  }
+
+  //Dynamically determine what type of result text to display
+  let resultText = (
+    <Text style={styles.resultText}>Roll the Dice and Make a Wager</Text>
+  )
+
+  const userGuessNum = parseInt(userGuess);
+  const userWagerNum = parseInt(userWager);
+  const diceSumNum = parseInt(diceSum);
+  if (userGuess !== "" && userGuessNum === diceSumNum) {
+    resultText = <Text style={styles.resultText}>You Won ${(userWagerNum * 5).toFixed(2)}</Text>
+  };
+  if (userGuess !== "" && userGuessNum !== diceSumNum) {
+    resultText = <Text style={styles.resultText}>You Lost ${(userWagerNum)}</Text>
+  };
+
+
+  return (
+    <>
+      <StatusBar style="auto" />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.titleContainer}>
+          <View style={styles.titleBackground}>
+            <Text style={styles.title}>Dice Roller</Text>
+          </View>
+        </View>
+
+        <View style={styles.rollButtonContainer}>
+          <Pressable
+          // Add the android ripple
+          android_ripple={{color: "#681313"}}
+          // Style the button when pressed
+          style={({pressed}) => pressed && styles.pressedButton}
+          //Whe Pressed open modal screen
+          onPress={startDiceRollHandler}
+          >
+            <View style={styles.rollButton}>
+              <Text style={styles.rollButtonText}>Roll Dice</Text>
+            </View>
+          </Pressable>
+          {/* <Button title="Roll Dice" style={styles.rollButton} /> */}
+        </View>
+
+        <View style={styles.diceContainer}>
+          <View style={styles.dice}>
+            <Text style={styles.diceNumber}>{dice1}</Text>
+          </View>
+          <View style={styles.dice}>
+            <Text style={styles.diceNumber}>{dice2}</Text>
+          </View>
+        </View>
+
+        <View style={styles.resultsContainer}>
+          <Text style={styles.resultText}>The resulting dice roll is {diceSum}</Text>
+        </View>
+
+        <View style={styles.resultsContainer}>
+          <Text style={styles.resultText}>{resultText}</Text>
+        </View>
+
+
+        <Modal visible={modalIsVisible} animationType="slide">
+          <SafeAreaView style={styles.modalRoot}>
+            <Text style={styles.inputLabel}>Guess the Roll Value:</Text>
+            <TextInput
+            style={styles.textInput}
+            //placeholder for when it's empty
+            placeholder="Enter A Guess Between 2 and 12" 
+            //set teh keyboard type to number pad for only integers
+            keyboardType="number-pad"
+            //when the text is changed, update the usersGuess
+            onChangeText={setUserGuess}
+            //Tie the entered value to the userGuess so when
+            //it is reset to blank the input field will also reset
+            value={userGuess}
+            />
+            <Text style={styles.inputLabel}>What's Your Wager:</Text>
+            <TextInput 
+            style={styles.textInput}
+            //placeholder for when it's empty
+            placeholder="Enter A Your Wager Here" 
+            //set teh keyboard type to number pad for only integers
+            keyboardType="number-pad"
+            //when the text is changed, update the usersGuess
+            onChangeText={setUserWager}
+            //Tie the entered value to the userGuess so when
+            //it is reset to blank the input field will also reset
+            value={userWager}
+            />
+
+            <View style={styles.modalButtonContainer}>
+              <View style={styles.modalButton}>
+                <Button title="Roll Dice" onPress={onDiceRoll}/>
+              </View>
+              <View style={styles.modalButton}>
+                <Button title="Cancel" color="black" onPress={endDiceRollHandler}/>
+              </View>
+            </View>
+
+          </SafeAreaView>
+        </Modal>
+
+
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#8849b3",
+    alignItems: "center",
+    justifyContent: "center", 
+    paddingTop: 40
+  },
+  titleContainer: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  titleBackground: {
+    backgroundColor: "white",
+    padding: 20,
+    paddingHorizontal: 20,
+    borderWidth: 3,
+    borderRadius: 7
+  },
+  title: {
+    fontSize: 60,
+    fontWeight: "bold"
+  },
+  rollButtonContainer: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  pressedButton: {
+    opacity: .8
+  },
+  rollButton: {
+    backgroundColor: "white",
+    padding: 10,
+    paddingHorizontal: 20,
+    borderWidth: 3,
+    borderRadius: 7
+  },
+  rollButtonText: {
+    fontSize: 25
+  },
+  diceContainer: {
+    flex: 3,
+    flexDirection: "row",
+    alignContent: "space-between",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80%"
+  },
+  dice: {
+    backgroundColor: "white",
+    borderWidth: 3,
+    borderRadius: 7,
+    margin: 20,
+    width: 100,
+    height: 100,
+    justifyContent: "center", 
+    alignItems: "center",
+
+  },
+  diceNumber: {
+    fontSize: 40,
+    fontWeight: "bold",
+    fontStyle: "italic"
+  },
+  resultsContainer: {
+    flex: 1,
+
+  },
+  resultText: {
+    fontSize: 20
+  },
+  modalRoot: {
+    flex: 1,
+    backgroundColor: "#8849b3",
+    alignItems: "center",
+  },
+  inputLabel: {
+    fontSize: 25,
+    color: "white",
+    marginTop: 20
+  },
+  textInput: {
+    backgroundColor: "#ffd4d4",
+    borderWidth: 1,
+    borderRadius: 6,
+    padding: 10,
+    color: "#8849b3",
+    marginBottom: 30
+  },
+  modalButtonContainer: {
+    flexDirection: "row",
+    marginTop: 16,
+
+  },
+  modalButton: {
+    width: "30%",
+    marginHorizontal: 8
+  }
+});
